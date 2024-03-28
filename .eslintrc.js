000361d6eb458acc6ae3285ca429a635cddf16fc
Sergio <seguijarr@alumnos.unex.es>
2024-03-28 12:32:52 +0100

    antes de arreglar warnings 2803

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    'react-native/react-native': true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+  }
 };
